eeb2eb34bcfb181906e83660298161640a89f0c7
shovonroy173 <shovonroy2003@gmail.com>
2024-05-06 10:31:39 +0530

    modi: modal update

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./modal.css";
-const Modal = () => {
+import CloseIcon from '@mui/icons-material/Close';
+import axios from "axios";
+const Modal = ({setOpen , userId , postId}) => {
+  console.log(userId , postId);
+  const [comments , setComments] = useState([]);
+  useEffect(()=>{
+    const getComments = async()=>{
+      const res = await axios.get(`http://localhost:5000/api/comment/getComments/${postId}`);
+      setComments(res.data);
+    };
+    getComments();
+  } , [postId]);
+  console.log(comments);
   return (
     <div className="modalContainer">
-      <div className="modalContent">Modal</div>
+      <div className="modalContent">
+        <div>
+          {comments.map((item , index)=>(
+            <p className="" key={index}>{item?.comment}</p>
+          ))}
+        </div>
+      <CloseIcon onClick={()=>setOpen(false)} style={{"color":"black"}}/>
+
+      </div>
+
     </div>
   );
 };
